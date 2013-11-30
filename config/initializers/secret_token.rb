bb51ab8aa282757a48ffcfc4d3cd03fa2231d2c9
intenex <intenex@gmail.com>
2013-11-30 15:08:39 -0500

    initial rails install

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Emailgeddon::Application.config.secret_key_base = '9290d7bc5b0dd8e23ccbc36a3355f5e873718051836ad5a6e4612179f0c16ae7c8fb2a22c8ae766a3587180e3d05e2984aba4db818200ea1b270c389070a42a8'
