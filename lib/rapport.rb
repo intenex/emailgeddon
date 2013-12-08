c048848a25e9708393cd096c3f51d521d2589c87
intenex <intenex@gmail.com>
2013-12-08 01:29:22 -0500

    mmmmm

---
+++ lib/rapport.rb
@@ -0,0 +1,38 @@
+require 'httpclient'
+require 'json'
+
+def find_email(email)
+    login_url = 'https://rapportive.com/login_status?user_email=' + email
+    profile_url = 'https://profiles.rapportive.com/contacts/email/' + email
+    
+    # create a new instance of the HTTPClient
+    c = HTTPClient.new
+    # connect to rapportive to get the session token
+    # Parsing JSON to ruby hash: http://stackoverflow.com/a/5410713/674794
+    login_result = JSON.parse(c.get_content(login_url))
+    session_token = login_result['session_token']
+    # set an external header value to pass in the session_token
+    ext_header = {'X-Session-Token' => session_token}
+    
+    # send result with the session_id token in the header and an empty query
+    # note that can also use c.get() instead of c.get_content, but then have to JSON.parse() the result.body separately
+    profile_result = JSON.parse(c.get_content(profile_url, '', ext_header))
+    # all contact info is saved under the attribute 'contact'
+    contact_info = profile_result['contact']
+    
+    person = Hash.new
+    person['name'] = contact_info['name']
+    person['jobs'] = Array.new
+    contact_info['occupations'].each do |job|
+        person['jobs'].push(job['job_title'] + " at " + job['company'])
+    end
+    person['sites'] = Array.new
+    contact_info['memberships'].each do |site|
+        person['sites'].push(site['site_name'] + ": " + site['profile_url'])
+    end
+    
+    puts person['name'] + "\n\n"
+    person['jobs'].each { |job| puts job + "\n" }
+    puts "\n"
+    person['sites'].each { |site| puts site + "\n" }
+end
